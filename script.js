b5fc73ee0b12483c4fcb5882093edb86900952c8
ShameenaKoodan <shameena.koodan@gmail.com>
2022-08-04 14:15:25 -0500

    Updated the display function to dynamically generate labels for the clue Word

--- script.js
+++ script.js
@@ -5,14 +5,16 @@
 const abcButtons = document.querySelectorAll('.abc-buttons');
 
 //Label for the words
-const wordLabel = document.querySelector('#character-words');
+const wordLabel = document.querySelector('.character-words');
 
+//div to display labels
+const labelDiv = document.querySelector(".flexcontainer-playarea-word");
 /****************************************************************
                      Variable Declarations
 *****************************************************************/
 //Word display
-const originalWord = "apple";
-const clue = ['p'];
+const originalWord = "fantastic";
+const clue =['a'];
 
 /****************************************************************
                      Function Definitions
@@ -20,24 +22,42 @@ const clue = ['p'];
 
 //Function to display a word with only hints and underscores on the label
 const displayClueWord = ()=>{
-       wordLabel.innerHTML = `${clue}`;
+       //Generate random number 
+       const randomIndex = Math.floor(Math.random() * originalWord.length);
+
+       //Dynamically create labels for each characters in the clue word
+       for (let i = 0; i < originalWord.length; i++) {
+              const label = document.createElement("label");
+              //Add class to the newly created labels
+              label.classList.add('character-words');
+              if(i==randomIndex){
+                     const t = document.createTextNode(originalWord[i]);
+                     label.appendChild(t);
+              labelDiv.appendChild(label);
+              }else{
+                     const t = document.createTextNode(" _ ");
+                     label.appendChild(t);
+              labelDiv.appendChild(label);
+              }
+              
+       }
+    //   wordLabel.innerHTML = `${clue}`;
 }
 
 //Function to display letters on label when the button is clicked
 const displayLetters=(event)=>{
     const alphabetClicked = event.target.innerHTML;
-    console.log(alphabetClicked);
 
     //Find all the indexes of the character which is clicked
     const indexes = getAllIndexes(originalWord,alphabetClicked);
        console.log(indexes);       
     //Add characters to the correct position in the clueWord if the clicked character is present
-    for(let i =0;i<indexes.length;i++){
-       clue[indexes[i]] = alphabetClicked;
-       displayClueWord();
-       console.log(clue);
-    }
-    
+       indexes.forEach(element => {
+              console.log(`Clue before insertion ${clue}`);
+              clue[element] = alphabetClicked;
+              console.log(`Clue after insertion ${clue}`); 
+       });
+       wordLabel.innerHTML = `${clue}`;
 }
 
 //Function to find all the index of a character in the word
